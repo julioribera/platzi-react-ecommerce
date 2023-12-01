204b572ad8218d76bb9598e70650d87251756610
Julio Ribera <julio@magiaycardistry.com>
2023-12-01 09:35:30 +0100

    Añadir al carrito en product detail

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -1,14 +1,15 @@
-export const ProductDetail = ({ product, onClose }) => {
+import {useAddToCart} from "../../Context/addToCart";
+
+export const ProductDetail = ({product, onClose}) => {
+  const addToCart = useAddToCart();
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex justify-center items-center">
       <aside
         className="relative flex flex-col bg-white border border-gray-300 rounded-lg text-lg"
-        style={{ width: "500px", height: "fit-content" }}
-      >
+        style={{width: "500px", height: "fit-content"}}>
         <button
           onClick={onClose}
-          className="absolute top-0 right-0 text-lg font-semibold p-2 z-30"
-        >
+          className="absolute top-0 right-0 text-lg font-semibold p-2 z-30">
           X
         </button>
         <img
@@ -22,7 +23,9 @@ export const ProductDetail = ({ product, onClose }) => {
             {product.price}
           </h2>
           <p className="text-gray-600">{product.description}</p>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4 transition">
+          <button
+            onClick={() => addToCart(product)}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4 transition">
             Añadir al carrito
           </button>
         </div>
--- src/Pages/App/ComponentLoader.jsx
+++ src/Pages/App/ComponentLoader.jsx
@@ -1,7 +1,7 @@
-import React, { lazy, Suspense } from "react";
+import React, {lazy, Suspense} from "react";
 
 const ComponentLoader = (componentName) => {
-  const Component = lazy(() => import(`../${componentName}`));
+  const Component = lazy(() => import(`../${componentName}/index.jsx`));
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Component />
